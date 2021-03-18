66bfaebf6a394c45e5321b8b6f25db761e31fd79
Malte Schmitz <schmitz@isp.uni-luebeck.de>
2021-03-18 20:31:10 +0100

    Remove test prefix from method name

--- src/test/java/schach/ExampleTest.java
+++ src/test/java/schach/ExampleTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ExampleTest {
     @Test
-    public void testFoo() {
+    public void foo() {
         Example example = new Example(23);
         assertEquals(42, example.foo(12), "foo performs a correct computation");
     }
