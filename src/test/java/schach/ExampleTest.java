c886e8a494e684ec01bcf21b8ce7fbd11b722c36
Malte Schmitz <schmitz@isp.uni-luebeck.de>
2021-03-24 18:10:49 +0100

    Add javadoc to example test case

--- src/test/java/schach/ExampleTest.java
+++ src/test/java/schach/ExampleTest.java
@@ -4,7 +4,13 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+/**
+ * Testing the Example class with its arbitrary arithmetical computations
+ */
 public class ExampleTest {
+    /**
+     * This tes case doesn't make any sense. It is just here as an example.
+     */
     @Test
     public void foo() {
         Example example = new Example(23);
