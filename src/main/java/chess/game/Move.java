f33057432aaad6f994521bc96c9b9d9b45d7dffa
Christian Stelter <christian.stelter@student.uni-luebeck.de>
2021-04-30 12:12:39 +0200

    Merge branch 'gameBranch'
    # Conflicts:
    #	src/main/java/chess/pieces/Bishop.java
    #	src/main/java/chess/pieces/King.java
    #	src/main/java/chess/pieces/Knight.java
    #	src/main/java/chess/pieces/Pawn.java
    #	src/main/java/chess/pieces/Piece.java
    #	src/main/java/chess/pieces/Queen.java
    #	src/main/java/chess/pieces/Rook.java

--- src/main/java/chess/game/Move.java
+++ src/main/java/chess/game/Move.java
@@ -1,11 +1,14 @@
 package chess.game;
 
+import chess.pieces.Piece;
+
 /**
  * Move class to calculate and save the movement of a Chess-Piece.
  */
 public class Move {
     public Square startSquare;
     public Square finalSquare;
+    public Piece movingPiece;
 
     /**
      * Constructor of Move-Class.
@@ -16,5 +19,37 @@ public class Move {
     public Move(Square start, Square finish) {
         this.startSquare = start;
         this.finalSquare = finish;
+        this.movingPiece = start.occupiedBy;
     }
+
+    /**
+     * Checks if Console Input is a syntactical correct Move.
+     *
+     * @param consoleInput Input of active Player as a String.
+     *
+     * @return boolean
+     */
+    public boolean validMove(String consoleInput){
+        if (consoleInput.charAt(2) == '-'){
+            if(Label.contains(consoleInput.substring(0, 2)) && Label.contains(consoleInput.substring(3, 5))){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Checks if Console Input is a semantically correct Move.
+     * TODO
+     * @param consoleInput Input of active Player as a String.
+     *
+     * @return boolean
+     */
+    public boolean allowedMove(String consoleInput){
+
+
+        return true;
+
+    }
+
 }
