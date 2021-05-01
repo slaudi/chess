1bc31d60b48fe4776a9642f6b63e53b280f11313
Christian Stelter <christian.stelter@student.uni-luebeck.de>
2021-05-01 09:05:27 +0200

    edited Square class: added x- and y-coordinates to Square-constructor

--- src/main/java/chess/game/Square.java
+++ src/main/java/chess/game/Square.java
@@ -6,6 +6,8 @@ import chess.pieces.Piece;
  * Square class representing a single square of current Chess-Board.
  */
 public class Square {
+    public int x;
+    public int y;
     public Label label;
     public boolean occupied;
     public Piece occupiedBy;
@@ -14,8 +16,12 @@ public class Square {
      * Create a single square instance when starting a new Game.
      *
      * @param label Name of Square.
+     * @param x X-Coordinate of this Square (line from left to right).
+     * @param y Y-Coordinate of this Square (column from top to bottom).
      */
-    public Square(Label label) {
+    public Square(Label label, int x, int y) {
+        this.x = x;
+        this.y = y;
         this.label = label;
         this.occupied = false;
         this.occupiedBy = null;
