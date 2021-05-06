81c35a1c13cdf1bba344c130df63df8bbc99a8e5
Sara Laudien <sara.laudien@student.uni-luebeck.de>
2021-05-06 18:40:11 +0200

    Changed CLi to handle all input and output

--- src/main/java/chess/cli/Cli.java
+++ src/main/java/chess/cli/Cli.java
@@ -1,7 +1,8 @@
 package chess.cli;
 
 import chess.game.Game;
-import chess.game.Move;
+import chess.game.Square;
+import chess.pieces.Piece;
 
 import java.util.Scanner;
 
@@ -9,20 +10,53 @@ import java.util.Scanner;
  * Starting point of the command line interface
  */
 public class Cli {
+
+    // TODO: ein Spieler gibt auf!
     /**
      * The entry point of the CLI application.
      *
      * @param args The command line arguments passed to the application
      */
     public static void main(String[] args) {
-        System.out.println("Läuft bis hier hin schon mal");
         Game currentGame = new Game();
-        currentGame.processGame();
+        currentGame.board.toConsole();
+
+        while(!currentGame.currentPlayer.getLoser() /*&& !currentGame.isADraw() && !aufgegeben*/) {
+
+            System.out.println("Now playing as " + currentGame.currentPlayer.getColour());
+            String userInput = getInput();
+
+            while (!generateAnswer(userInput, currentGame)) {
+                userInput = getInput();
+            }
+
+            Square startSquare = currentGame.board.getStartSquareFromInput(userInput);
+            Square finalSquare = currentGame.board.getFinalSquareFromInput(userInput);
+
+            if (!currentGame.processMove(startSquare, finalSquare)) {
+                // goes back to the beginning of the while loop, doesn't switch players or redraws board
+                continue;
+            }
+            currentGame.currentPlayer = currentGame.currentPlayer == currentGame.playerWhite ?
+                    currentGame.playerBlack : currentGame.playerWhite;
+
+            currentGame.board.toConsole();
+        }
+
+        if(currentGame.currentPlayer.getLoser()) {
+            System.out.println(currentGame.currentPlayer.getColour() + " is Loser!");
+        } else /*if(currentGame.isADraw())*/{
+            System.out.println("The game ended in a draw!");
+        } /*else {
+            System.out.println(currentGame.currentPlayer.getColour() + " has given up!");
+            }*/
+
     }
 
     /**
-     * Gets input as String from console.
-     * @return Input from console as String.
+     * Gets input as a String from the console.
+     *
+     * @return a String of the input
      */
     public static String getInput() {
         Scanner scanner = new Scanner(System.in);
@@ -32,17 +66,45 @@ public class Cli {
 
     /**
      * Computes an answer based on Console-Input and state of current game.
+     *
      * @param input Input from Console as a String
-     * @param game Current game.
-     * @return An answer to the Player if his entered Move is syntactically or semantically correct or incorrect.
+     * @param currentGame the current game
+     * @return a boolean indicating if the move is accepted
      */
-    public static String generateAnswer(String input, Game game){
-        if(Move.isValidMove(input)){
-            if (game.board.getMovingPieceFromInput(input).isAllowedPath(game.board.getFinalSquareFromInput(input))){
-                return "!" + input;
-            }
-            else return "!Move not allowed";
+    public static boolean generateAnswer(String input, Game currentGame) {
+        Piece selectedPiece = currentGame.board.getMovingPieceFromInput(input);
+        Piece targetPiece = currentGame.board.getFinalSquareFromInput(input).getOccupiedBy();
+        Square finalSquare = currentGame.board.getFinalSquareFromInput(input);
+
+        // TODO: funktioniert noch nicht
+        String beaten = "beaten";
+        if (input.equals(beaten)) {
+            System.out.println("Beaten pieces:" + currentGame.beatenPieces);
+            return false;
         }
-        else return "!Invalid move";
+        if(currentGame.isValidMove(input)){
+            if (selectedPiece == null) {
+                System.out.println("There is no Piece to move!");
+                return false;
+            }
+            if (selectedPiece.getColour() != currentGame.currentPlayer.getColour()) {
+                System.out.println("This is not your Piece to move!");
+                return false;
+            }
+            if (targetPiece != null && targetPiece.getColour() == currentGame.currentPlayer.getColour()) {
+                System.out.println("You cannot attack your own Piece!");
+                return false;
+            }
+            if (selectedPiece.isAllowedPath(finalSquare)/*currentGame.isMoveAllowed(selectedPiece, finalSquare)*/){
+                System.out.println("!" + input);
+                return true;
+            } else {
+                System.out.println("!MoveNotAllowed");
+                return false;
+            }
+        } else {
+            System.out.println("!InvalidMove");
+            return false;
+        }
     }
 }
