07361239fee51f19f7d1df1b8e3ae3af86f043f6
Sara Laudien <sara.laudien@student.uni-luebeck.de>
2021-05-11 13:37:25 +0200

    Deleted not used methods 'getXFromLabel' and 'getYFromLabel'

--- src/main/java/chess/game/Square.java
+++ src/main/java/chess/game/Square.java
@@ -53,45 +53,6 @@ public class Square {
     public void setOccupiedBy(Piece occupiedBy) {
         this.occupiedBy = occupiedBy;
     }
-    /**
-     * Get x-Coordinate from Enum Label
-     * @param label Enum to extract x-Coordinate from.
-     * @return x-Coordinate from Enum.
-     */
-    public static int getXFromLabel(Label label){
-        char i = Label.toString(label).charAt(0);
-        int result;
-        if (i == 'a') result = 0;
-        else if (i == 'b') result = 1;
-        else if (i == 'c') result = 2;
-        else if (i == 'd') result = 3;
-        else if (i == 'e') result = 4;
-        else if (i == 'f') result = 5;
-        else if (i == 'g') result = 6;
-        else if (i == 'h') result = 7;
-        else result = 9;
-        return result;
-    }
-
-    /**
-     * Get y-Coordinate from Enum Label
-     * @param label Enum to extract y-Coordinate from.
-     * @return y-Coordinate from Enum.
-     */
-    public static int getYFromLabel(Label label){
-        char i = Label.toString(label).charAt(1);
-        int result;
-        if (i == '8') result = 0;
-        else if (i == '7') result = 1;
-        else if (i == '6') result = 2;
-        else if (i == '5') result = 3;
-        else if (i == '4') result = 4;
-        else if (i == '3') result = 5;
-        else if (i == '2') result = 6;
-        else if (i == '1') result = 7;
-        else result = 9;
-        return result;
-    }
 
     /**
      * Get x-Coordinate from String
