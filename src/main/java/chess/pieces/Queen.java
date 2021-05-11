0cd91a3c15b65caa84a5023929bc455c8dac37bc
Sara Laudien <sara.laudien@student.uni-luebeck.de>
2021-05-11 13:36:40 +0200

    Changed the return of isPiecesMove, so Queen can not only move diagonally

--- src/main/java/chess/pieces/Queen.java
+++ src/main/java/chess/pieces/Queen.java
@@ -73,7 +73,7 @@ public class Queen extends Piece {
         int diff_x = Math.abs(finalSquare.getX() - this.square.getX());
         int diff_y = Math.abs(finalSquare.getY() - this.square.getY());
 
-        return (diff_x == diff_y);
+        return (diff_x == diff_y || diff_y == 0 || diff_x == 0);
     }
 
 }
