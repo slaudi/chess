712bb2915a1ca6e069b3706be4f91de3d993bc54
Christian Stelter <christian.stelter@student.uni-luebeck.de>
2021-05-13 21:56:14 +0200

    added Board tests

--- src/test/java/chess/game/BoardTest.java
+++ src/test/java/chess/game/BoardTest.java
@@ -11,39 +11,66 @@ import static org.junit.jupiter.api.Assertions.*;
 /**
  * The BoardTest class test the methods of the Board class
  */
-class BoardTest {
+public class BoardTest {
 
-    private Square[][] board;
-    private Game game;
+    public Square[][] board;
+    public Game game;
 
     @BeforeEach
-    void setUp() {
+    public void setUp() {
         game = new Game();
     }
 
     @Test
-    void getBoard() {}
-
-    @Test
-    void startingFormation() {
+    public void startingFormation() {
     }
 
     @Test
-    void toConsole() {
+    public void toConsole() {
     }
 
     @Test
-    void getMovingPieceFromInput() {}
-
-    @Test
-    void getStartSquareFromInput() {}
-
-    @Test
-    void getFinalSquareFromInput() {
+    public void getMovingPieceFromInput() {
+        String input = "b7-b6";
+        assertEquals(this.game.chessBoard.getChessBoard()[1][1].getOccupiedBy(), this.game.chessBoard.getMovingPieceFromInput(input));
     }
 
     @Test
-    void getSquareOfKing() {
+    public void getStartSquareFromInput() {
+        String input = "a8-a7";
+        assertEquals(this.board[0][0], this.game.chessBoard.getStartSquareFromInput(input));
+    }
 
+    @Test
+    public void getFinalSquareFromInput() {
+        String input = "a7-a8";
+        assertEquals(this.board[0][0], this.game.chessBoard.getFinalSquareFromInput(input));
+    }
+
+    @Test
+    public void getSquareOfKing() {
+        Square king = this.board[4][7];
+        assertEquals(king, this.game.chessBoard.getSquareOfKing(Colour.WHITE));
+    }
+
+    @Test
+    public void getChessBoard() {
+
+    }
+
+    @Test
+    public void getWhiteAlliance() {
+        assertEquals(this.game.chessBoard.whitePieces, this.game.chessBoard.getWhiteAlliance());
+    }
+
+    @Test
+    public void getBlackAlliance() {
+        assertEquals(this.game.chessBoard.blackPieces, this.game.chessBoard.getBlackAlliance());
+    }
+
+    @Test
+    public void getPromotionKey() {
+        String input = "e7-e8Q";
+        assertEquals('Q', this.game.chessBoard.getPromotionKey(input));
     }
 }
