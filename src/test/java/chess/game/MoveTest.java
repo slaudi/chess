a895aed0d90c0f7f06d4202092e99779480d7749
Christian Stelter <christian.stelter@student.uni-luebeck.de>
2021-05-15 19:07:59 +0200

    added moveTests

--- src/test/java/chess/game/MoveTest.java
+++ src/test/java/chess/game/MoveTest.java
@@ -1,5 +1,6 @@
 package chess.game;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,7 +10,62 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 class MoveTest {
 
+    public Game game;
+    public Move move;
+    public Move move2;
+
+    @BeforeEach
+    public void setUp() {
+        game = new Game();
+        move = new Move(game.chessBoard.getStartSquareFromInput("a2-a3"), game.chessBoard.getFinalSquareFromInput("a2-a3"));
+        move2 = new Move(game.chessBoard.getStartSquareFromInput("a3-a2"), game.chessBoard.getFinalSquareFromInput("a3-a2"));
+    }
+
     @Test
-    void validMove() {
+    public void getStartSquare() {
+        assertEquals(game.chessBoard.getStartSquareFromInput("a2-a3"), move.getStartSquare());
+    }
+
+    @Test
+    public void getFinalSquare() {
+        assertEquals(game.chessBoard.getFinalSquareFromInput("a2-a3"), move.getFinalSquare());
+    }
+
+    @Test
+    public void doMove() {
+        move.doMove(game.chessBoard);
+        move2.doMove(game.chessBoard);
+        assertEquals(game.chessBoard, game.chessBoard);
+    }
+
+    @Test
+    public void undoMove() {
+        move.doMove(game.chessBoard);
+        move.undoMove(move, game.chessBoard);
+        assertEquals(game.chessBoard, game.chessBoard);
+    }
+
+    @Test
+    public void castlingMove() {
+        move.castlingMove(game.chessBoard);
+        Board board2 = game.chessBoard;
+        move2.castlingMove(game.chessBoard);
+        assertNotEquals(game.chessBoard, board2);
+    }
+
+    @Test
+    public void enPassantMove() {
+    }
+
+    @Test
+    public void undoEnPassant() {
+    }
+
+    @Test
+    public void doPromotion() {
+    }
+
+    @Test
+    public void undoPromotion() {
     }
 }
