1621b7aaa0af5cb4a4306a26e0611dd2825b2667
Christian Stelter <christian.stelter@student.uni-luebeck.de>
2021-05-15 19:05:17 +0200

    added knightTests

--- src/test/java/chess/pieces/KnightTest.java
+++ src/test/java/chess/pieces/KnightTest.java
@@ -1,65 +1,85 @@
 package chess.pieces;
 
 import chess.game.Colour;
+import chess.game.Game;
 import chess.game.Square;
 import chess.game.Type;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static chess.game.Label.c4;
-import static chess.game.Label.g5;
+import static chess.game.Label.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * The KnightTest class test the methods of the Knight class
  */
 public class KnightTest {
-    private Knight knight;
-    private Square square;
+    public Game game;
+    public Knight knight;
+    public Square squareC4;
+    public Square squareF4;
+    public Square squareH6;
+    public Square squareE6;
+    public Square squareB2;
 
     @BeforeEach
-    void setUp() {
-        square = new Square(c4, 2, 4);
-        knight = new Knight(square, Colour.WHITE);
+    public void setUp() {
+        game = new Game();
+        squareC4 = new Square(c4, 2, 4);
+        knight = new Knight(squareC4, Colour.WHITE);
+        squareF4 = new Square(f4, 5, 4);
+        squareH6 = new Square(h6, 7, 2);
+        squareE6 = new Square(e6, 4, 2);
+        squareB2 = new Square(b2, 1, 6);
     }
 
     @Test
-    void getSquare() {
-        assertEquals(square, knight.getSquare());
+    public void getSquare() {
+        assertEquals(squareC4, knight.getSquare());
     }
 
     @Test
-    void setSquare() {
-        square = new Square(g5,7,3);
-        knight.setSquare(square);
-        assertEquals(square, knight.getSquare());
+    public void setSquare() {
+        squareC4 = new Square(g5,7,3);
+        knight.setSquare(squareC4);
+        assertEquals(squareC4, knight.getSquare());
     }
 
     @Test
-    void getColour() {
+    public void getColour() {
         assertEquals(Colour.WHITE, knight.getColour());
     }
 
     @Test
-    void getType() {
+    public void getType() {
         assertEquals(Type.KNIGHT, knight.getType());
     }
 
     @Test
-    void getHasMoved() {
+    public void getHasMoved() {
         assertFalse(knight.isHasMoved());
     }
 
     @Test
-    void setHasMoved() {
+    public void setHasMoved() {
         knight.setHasMoved(true);
         assertTrue(knight.isHasMoved());
     }
 
-    // TODO: test moves of the pieces
     @Test
-    void isPiecesMove() {
+    public void isPiecesMove() {
+        assertTrue(knight.isPiecesMove(squareB2));
+    }
 
+    @Test
+    public void isHasMoved() {
+        knight.setHasMoved(true);
+        assertTrue(knight.isHasMoved());
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals("N", knight.toString());
     }
 }
