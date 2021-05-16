8ba67c17bcccd8e9ff1458f4eeb8dbeb187f9a00
Sara Laudien <sara.laudien@student.uni-luebeck.de>
2021-05-16 15:26:06 +0200

    Added/changed tests

--- src/test/java/chess/game/PlayerTest.java
+++ src/test/java/chess/game/PlayerTest.java
@@ -1,5 +1,7 @@
 package chess.game;
 
+import chess.pieces.Piece;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -9,59 +11,71 @@ import static org.junit.jupiter.api.Assertions.*;
  * The PlayerTest class test the methods of the Player class
  */
 class PlayerTest {
-    public Player playerB;
-    public Player playerW;
     public Game game;
+    public Piece pawn;
 
     @BeforeEach
     public void setUp() {
-        playerB = new Player(Colour.BLACK);
-        playerW = new Player(Colour.WHITE);
         game = new Game();
-    }
-
-    @Test
-    public void setColour() {
-        playerB.setColour(Colour.WHITE);
-        assertEquals(Colour.WHITE, playerB.getColour());
-    }
-
-    @Test
-    public void setInCheck() {
-        playerB.setInCheck(true);
-        assertTrue(playerB.inCheck);
-    }
-
-    @Test
-    public void setLoser() {
-        playerW.setLoser(true);
-        assertTrue(playerW.loser);
+        pawn = game.chessBoard.getChessBoard()[0][6].getOccupiedBy();
+        game.getBeatenPieces().add(pawn);
     }
 
     @Test
     public void getColour() {
-        assertEquals(Colour.WHITE, playerW.getColour());
+        assertEquals(Colour.WHITE, game.currentPlayer.getColour());
+    }
+
+    @Test
+    public void setColour() {
+        game.currentPlayer.setColour(Colour.WHITE);
+        assertEquals(Colour.WHITE, game.currentPlayer.getColour());
+    }
+
+    @Test
+    public void setInCheck() {
+        game.currentPlayer.setInCheck(true);
+        assertTrue(game.currentPlayer.inCheck);
+    }
+
+    @Test
+    public void setLoser() {
+        game.currentPlayer.setLoser(true);
+        assertTrue(game.currentPlayer.loser);
     }
 
     @Test
     public void isInCheck() {
-        playerW.setInCheck(true);
-        assertTrue(playerW.isInCheck());
+        game.currentPlayer.setInCheck(true);
+        assertTrue(game.currentPlayer.isInCheck());
     }
 
     @Test
     public void isLoser() {
-        playerB.setLoser(true);
-        assertTrue(playerB.isLoser());
+        assertFalse(game.currentPlayer.isLoser());
+    }
+
+
+    @Test
+    public void notWhiteAlliedPieces() {
+        assertNotEquals(game.playerWhite.getAlliedPieces(game.getBeatenPieces(), game.chessBoard), game.playerBlack.getAlliedPieces(game.getBeatenPieces(), game.chessBoard));
     }
 
     @Test
-    public void getAlliedPieces() {
-        assertNotEquals(playerW.getAlliedPieces(game.getBeatenPieces(), game.chessBoard), playerB.getAlliedPieces(game.getBeatenPieces(), game.chessBoard));
+    public void notBlackAlliedPieces(){
+        assertNotEquals(game.playerBlack.getAlliedPieces(game.getBeatenPieces(), game.chessBoard), game.playerWhite.getAlliedPieces(game.getBeatenPieces(), game.chessBoard));
     }
 
     @Test
-    public void getEnemyPieces() {
-        assertNotEquals(playerW.getEnemyPieces(game.getBeatenPieces(), game.chessBoard), playerB.getEnemyPieces(game.getBeatenPieces(), game.chessBoard));
+    public void notWhiteEnemyPieces() {
+        assertNotEquals(game.playerWhite.getEnemyPieces(game.getBeatenPieces(), game.chessBoard), game.playerBlack.getEnemyPieces(game.getBeatenPieces(), game.chessBoard));
     }
+
+    @Test
+    public void notBlackEnemyPieces(){
+        assertNotEquals(game.playerBlack.getEnemyPieces(game.getBeatenPieces(), game.chessBoard), game.playerWhite.getEnemyPieces(game.getBeatenPieces(), game.chessBoard));
+    }
+
+
+
 }
