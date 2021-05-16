6b267c2eadc43328fe4be2f50644ff7924f5b8e6
Sara Laudien <sara.laudien@student.uni-luebeck.de>
2021-05-16 14:11:22 +0200

    Added 'containsNot' and 'notSameValue' and comments

--- src/test/java/chess/game/LabelTest.java
+++ src/test/java/chess/game/LabelTest.java
@@ -9,18 +9,35 @@ import static org.junit.jupiter.api.Assertions.*;
  */
 public class LabelTest {
 
+    /**
+     * Tests 'contains' if Label contains the element.
+     */
     @Test
     public void contains() {
         assertTrue(Label.contains("e1"));
     }
 
+    /**
+     * Tests 'contains' if Label doesn't contain the element.
+     */
     @Test
-    public void values() {
+    public void containsNot(){
+        assertFalse(Label.contains("i6"));
+    }
+
+    /**
+     * Tests if a value is correctly assigned.
+     */
+    @Test
+    public void sameValue() {
         assertEquals(Label.values()[0], Label.a8);
     }
 
+    /**
+     * Tests if a value is different values are recognised.
+     */
     @Test
-    public void valueOf() {
-        assertEquals(Label.valueOf("a8"), Label.a8);
+    public void notSameValue() {
+        assertNotEquals(Label.valueOf("d6"), Label.a8);
     }
 }
