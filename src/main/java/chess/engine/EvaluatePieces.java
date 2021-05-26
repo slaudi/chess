c6061f3afcf65c48cd97d37e5813eecc48171bff
Sara Laudien <sara.laudien@student.uni-luebeck.de>
2021-05-26 17:10:42 +0200

    Added evaluation for Pieces.

---
+++ src/main/java/chess/engine/EvaluatePieces.java
@@ -0,0 +1,55 @@
+package chess.engine;
+
+import chess.game.Board;
+import chess.game.Colour;
+import chess.pieces.Piece;
+
+/**
+ * Evaluates the value of the Pieces on the board.
+ */
+public class EvaluatePieces implements Evaluation{
+
+
+    @Override
+    public int evaluateBoard(Board currentBoard, Colour playerColour) {
+        int sum = 0;
+        for (int y = 0; y < currentBoard.getHeight(); y++) {
+            for (int x = 0; x < currentBoard.getWidth(); x++) {
+                Piece piece = currentBoard.getPieceAt(x,y);
+                if (piece != null) {
+                    int value = piecesValue(piece);
+                    if (piece.getColour() == playerColour) {
+                        sum += value;
+                    } else {
+                        sum -= value;
+                    }
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    /**
+     * Value of the different Pieces.
+     *
+     * @param piece The Piece being evaluated.
+     * @return int The value of the Piece.
+     */
+    private int piecesValue(Piece piece){
+        switch (piece.getType()) {
+            case PAWN:
+                return 1;
+            case KNIGHT:
+            case BISHOP:
+                return 3;
+            case ROOK:
+                return 5;
+            case QUEEN:
+                return 9;
+            default:
+                return 0;
+        }
+    }
+
+}
---
+++ src/main/java/chess/engine/Evaluation.java
@@ -0,0 +1,27 @@
+package chess.engine;
+
+import chess.game.Board;
+import chess.game.Colour;
+
+/**
+ * Evaluates the status of the game on the basis of the Pieces on the board.
+ */
+public interface Evaluation {
+
+    int best_Score = Integer.MAX_VALUE;
+
+    int worst_Score = Integer.MIN_VALUE;
+
+    int balanced_Score = 0;
+
+    /**
+     * Evaluates the current board based on the Pieces on it from the point of view
+     * of the passed players colour.
+     *
+      * @param currentBoard The current Board.
+     * @param playerColour  The colour of the Player who wants to evaluate the Board.
+     * @return int  Returns an int representing the value of the current board. The higher the
+     *              better for the player of the passed colour.
+     */
+    int evaluateBoard(Board currentBoard, Colour playerColour);
+}
