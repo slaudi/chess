8c3fe96f5762c03d845d403d53e7d43b3db07f9d
Sara Laudien <sara.laudien@student.uni-luebeck.de>
2021-05-29 21:38:58 +0200

    According to PMD.

--- src/main/java/chess/engine/Evaluation.java
+++ src/main/java/chess/engine/Evaluation.java
@@ -1,9 +1,5 @@
 package chess.engine;
 
-import chess.game.Board;
-import chess.game.Colour;
-import chess.game.Game;
-import chess.pieces.Piece;
 
 /**
  * Evaluates the status of the game on the basis of the Pieces on the board.
@@ -15,6 +11,5 @@ public interface Evaluation {
     int lowestScore = Integer.MIN_VALUE;
 
     int balancedScore = 0;
-
 
 }
