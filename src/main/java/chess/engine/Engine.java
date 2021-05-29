c454cf9d163bec05292ef8c552085edcdd32de1b
Sara Laudien <sara.laudien@student.uni-luebeck.de>
2021-05-29 21:42:03 +0200

    Possible entry point/interface for interaction with game.

--- src/main/java/chess/engine/Engine.java
+++ src/main/java/chess/engine/Engine.java
@@ -1,9 +1,24 @@
 package chess.engine;
 
+import chess.game.Game;
+import chess.game.Move;
+
 /**
  *
  */
 public class Engine {
+
+    private MinimaxAlgorithm minimax;
+
+    /**
+     *
+     * @param game
+     */
+    public void nextMove(Game game) {
+        Move nextMove = minimax.computeBestMove(game);
+        nextMove.doMove(game.chessBoard);
+    }
+
 
 }
 
