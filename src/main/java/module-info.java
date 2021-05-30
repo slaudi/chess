845b6f457a7802689450c13548a46a63c4459005
Sara Laudien <sara.laudien@student.uni-luebeck.de>
2021-05-30 18:15:06 +0200

    Added dependency to use fxml.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,8 +4,10 @@
 module chess {
     requires javafx.controls;
     requires transitive javafx.graphics;
+    requires javafx.fxml;
 
     opens chess.pieces;
+    opens chess.gui;
 
     exports chess.gui;
 }
