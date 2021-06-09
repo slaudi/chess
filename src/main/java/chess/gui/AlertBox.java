5c778a1c533993ac8e4eab1c0aa032db788a1a32
Sara Laudien <sara.laudien@student.uni-luebeck.de>
2021-06-09 15:50:49 +0200

    Added Javadoc comments.

--- src/main/java/chess/gui/AlertBox.java
+++ src/main/java/chess/gui/AlertBox.java
@@ -3,8 +3,18 @@ package chess.gui;
 import javafx.scene.control.Alert;
 import javafx.stage.Modality;
 
+/**
+ * This class creates a customised alert box.
+ */
 public class AlertBox {
 
+    /**
+     * Function which displays an alert window with the specified messages.
+     *
+     * @param title         The title of the alert box.
+     * @param headerText    The header text of the alert box.
+     * @param contentText   The content text of the alert box showcasing the specified message.
+     */
     public static void display(String title, String headerText, String contentText) {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
 
