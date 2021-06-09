afd4483db29a41fc543e7d0d2ae287d71b1866b5
Sara Laudien <sara.laudien@student.uni-luebeck.de>
2021-06-09 19:42:19 +0200

    Added classes for switching languages.

---
+++ src/main/java/chess/gui/EnglishGame.java
@@ -0,0 +1,128 @@
+package chess.gui;
+
+
+import chess.game.Colour;
+import chess.game.Square;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class EnglishGame extends BorderPane {
+
+    public ChessBoardView chessBoardView;
+    public GuiGame guiGame;
+    String white;
+    String black;
+    String highlight;
+    int fontSize = 17;
+
+
+    /**
+     *
+     * @param guiGame
+     */
+    public EnglishGame(GuiGame guiGame, ChessBoardView chessBoardView) {
+
+        this.white = "-fx-background-color: floralwhite";
+        this.black = "-fx-background-color: slategray";
+        this.highlight = "-fx-border-color: skyblue";
+
+        this.guiGame = guiGame;
+        this.chessBoardView = chessBoardView;
+    }
+
+
+    HBox generatePlayersMoveLabelBox(){
+        Label label = new Label(guiGame.game.currentPlayer.getColour().toString() + "s Turn");
+        if (guiGame.game.isCheckMate()) {
+            AlertBox.display("Game Information","CheckMate",guiGame.game.currentPlayer.getColour().toString() + " has lost the Game!");
+            label = new Label(guiGame.game.currentPlayer.getColour().toString() + " lost the Game!");
+        } else if (guiGame.game.isADraw() || guiGame.draw) {
+            AlertBox.display("Game Information","Draw","The Game ended in a draw!");
+            label = new Label("The Game ended in a draw!");
+        } else if (guiGame.hintInCheck && guiGame.game.currentPlayer.isInCheck()){
+            label = new Label(guiGame.game.currentPlayer.getColour().toString() + "s Turn -- " + guiGame.game.currentPlayer.getColour().toString() + " is in Check!");
+            AlertBox.display("Check Hint",null, guiGame.game.currentPlayer.getColour().toString() + " is in Check!");
+        }
+        label.setFont(new Font(fontSize));
+        return new HBox(label);
+    }
+
+    GridPane generateGrid(){
+        if (guiGame.getSquareStart() != null && guiGame.getSquareFinal() == null && guiGame.highlightPossibleMoves) {
+            System.out.println(guiGame.getSquareStart().getLabel());
+            if(guiGame.getSquareStart().getOccupiedBy() != null){
+                System.out.println(guiGame.getSquareStart().getOccupiedBy());
+                if(guiGame.game.currentPlayer.getColour() == Colour.WHITE && guiGame.getSquareStart().getOccupiedBy().getColour() == Colour.WHITE) {
+                    return chessBoardView.generateHighlightedButtonGrid();
+                } else if (guiGame.game.currentPlayer.getColour() == Colour.BLACK && guiGame.getSquareStart().getOccupiedBy().getColour() == Colour.BLACK) {
+                    return chessBoardView.generateHighlightedButtonGrid();
+                } else {
+                    if (!guiGame.game.isADraw() || !guiGame.game.isCheckMate()) {
+                        guiGame.setSquareStart(null);
+                        AlertBox.display("Piece problem", null, "Selected Piece is not your Colour!");
+                    }
+                }
+            } else {
+                if (!guiGame.game.isADraw() || !guiGame.game.isCheckMate()) {
+                    guiGame.setSquareStart(null);
+                    AlertBox.display("Piece Problem", null, "There is no Piece to move!");
+                }
+            }
+        }
+        // no highlighted moves
+        return chessBoardView.generateButtonGrid();
+    }
+
+    void noAllowedSquares(List<Square> allowedSquares) {
+        if (allowedSquares.isEmpty()) {
+            guiGame.setSquareStart(null);
+            AlertBox.display("No Moves Possible", null, "This Piece cannot move. Try another!");
+        }
+    }
+
+    void generateAnswer(int result) {
+        if (result == 1){
+            AlertBox.display("Movement Error",null,"Move not allowed: Your King would be in Check!");
+        }
+        else if (result == 2){
+            AlertBox.display("Movement Error",null,"Move not allowed: Not possible!");
+        }
+        else if (result == 5){
+            AlertBox.display("Game-Error",null,"Something unexpected happened!?");
+        }
+    }
+
+
+
+    char promotionSelection() {
+        ButtonType buttonTypeOne = new ButtonType("Rook");
+        ButtonType buttonTypeTwo = new ButtonType("Knight");
+        ButtonType buttonTypeThree = new ButtonType("Bishop");
+        ButtonType buttonTypeFour = new ButtonType("Queen");
+
+        List<ButtonType> options = new ArrayList<>();
+        Collections.addAll(options,buttonTypeOne,buttonTypeTwo,buttonTypeThree,buttonTypeFour);
+
+        ButtonType buttonType = OptionBox.display("Promotion Options",null,"Your Pawn changes to:",options);
+
+        if (buttonType == buttonTypeOne) {
+            return 'R';
+        } else if (buttonType == buttonTypeTwo) {
+            return 'N';
+        } else if (buttonType == buttonTypeThree) {
+            return 'B';
+        } else {
+            return 'Q';
+        }
+    }
+
+
+}
---
+++ src/main/java/chess/gui/GermanGame.java
@@ -0,0 +1,142 @@
+package chess.gui;
+
+import chess.game.Colour;
+import chess.game.Square;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class GermanGame extends BorderPane {
+
+    public ChessBoardView chessBoardView;
+    public GuiGame guiGame;
+    String white;
+    String black;
+    String highlight;
+    int fontSize = 17;
+    String colour;
+
+
+
+    /**
+     *
+     * @param guiGame
+     */
+    public GermanGame(GuiGame guiGame, ChessBoardView chessBoardView) {
+
+        this.white = "-fx-background-color: floralwhite";
+        this.black = "-fx-background-color: slategray";
+        this.highlight = "-fx-border-color: skyblue";
+
+        this.guiGame = guiGame;
+        this.chessBoardView = chessBoardView;
+
+    }
+
+
+    HBox generatePlayersMoveLabelBox(){
+        Label label = new Label(getColour() + " ist am Zug");
+        if (guiGame.game.isCheckMate()) {
+            AlertBox.display("Spiel-Information","Schachmatt",getColour() + " hat das Spiel verloren!");
+            label = new Label(getColour() + " hat das Spiel verloren!");
+        } else if (guiGame.game.isADraw() || guiGame.draw) {
+            AlertBox.display("Spiel-Information","Unentschieden","Das Spiel endet in einem Unentschieden!");
+            label = new Label("Das Spiel endet in einem Unentschieden!");
+        } else if (guiGame.hintInCheck && guiGame.game.currentPlayer.isInCheck()){
+            label = new Label(getColour() + " ist am Zug -- " + getColour() + " steht im Schach!");
+            AlertBox.display("Schach-Hinweis",null, getColour() + " steht im Schach!");
+        }
+        label.setFont(new Font(fontSize));
+        return new HBox(label);
+    }
+
+    private String getColour() {
+        if (guiGame.game.currentPlayer.getColour() == Colour.BLACK) {
+            return "SCHWARZ";
+        } else {
+            return "WEISS";
+        }
+    }
+
+
+    GridPane generateGrid(){
+        if (guiGame.getSquareStart() != null && guiGame.getSquareFinal() == null && guiGame.highlightPossibleMoves) {
+            System.out.println(guiGame.getSquareStart().getLabel());
+            if(guiGame.getSquareStart().getOccupiedBy() != null){
+                System.out.println(guiGame.getSquareStart().getOccupiedBy());
+                if(guiGame.game.currentPlayer.getColour() == Colour.WHITE && guiGame.getSquareStart().getOccupiedBy().getColour() == Colour.WHITE) {
+                    return chessBoardView.generateHighlightedButtonGrid();
+                } else if (guiGame.game.currentPlayer.getColour() == Colour.BLACK && guiGame.getSquareStart().getOccupiedBy().getColour() == Colour.BLACK) {
+                    return chessBoardView.generateHighlightedButtonGrid();
+                } else {
+                    if (!guiGame.game.isADraw() || !guiGame.game.isCheckMate()) {
+                        guiGame.setSquareStart(null);
+                        AlertBox.display("Figuren-Problem", null, "Die ausgewählte Figur ist nicht deine Figur!");
+                    }
+                }
+            } else {
+                if (!guiGame.game.isADraw() || !guiGame.game.isCheckMate()) {
+                    guiGame.setSquareStart(null);
+                    AlertBox.display("Figuren-Problem", null, "Dort steht keine Figur zum Ziehen!");
+                }
+            }
+        }
+        // no highlighted moves
+        return chessBoardView.generateButtonGrid();
+    }
+
+
+    void noAllowedSquares(List<Square> allowedSquares) {
+        if (allowedSquares.isEmpty()) {
+            guiGame.setSquareStart(null);
+            AlertBox.display("Keine Züge möglich", null, "Diese Figur kann sich nicht bewegen. Versuch eine andere!!");
+        }
+    }
+
+
+
+    void generateAnswer(int result) {
+        if (result == 1){
+            AlertBox.display("Fehler",null,"Zug nicht erlaubt: Dein König wäre im Schach!");
+        }
+        else if (result == 2){
+            AlertBox.display("Fehler",null,"Zug nicht möglich!");
+        }
+        else if (result == 5){
+            AlertBox.display("Spiel-Fehler",null,"Etwas unerwartetes ist passiert!?");
+        }
+    }
+
+
+
+    char promotionSelection() {
+        ButtonType buttonTypeOne = new ButtonType("Turm");
+        ButtonType buttonTypeTwo = new ButtonType("Springer");
+        ButtonType buttonTypeThree = new ButtonType("Läufer");
+        ButtonType buttonTypeFour = new ButtonType("Königin");
+
+        List<ButtonType> options = new ArrayList<>();
+        Collections.addAll(options,buttonTypeOne,buttonTypeTwo,buttonTypeThree,buttonTypeFour);
+
+        ButtonType buttonType = OptionBox.display("Umwandlungs-Optionen",null,"Dein Bauer wird zu:",options);
+
+        if (buttonType == buttonTypeOne) {
+            return 'R';
+        } else if (buttonType == buttonTypeTwo) {
+            return 'N';
+        } else if (buttonType == buttonTypeThree) {
+            return 'B';
+        } else {
+            return 'Q';
+        }
+    }
+
+
+}
