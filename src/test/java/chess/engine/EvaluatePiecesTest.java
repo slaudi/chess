225b65dc686d4f6a70643591e4a54e811a380815
Christian Stelter <christian.stelter@student.uni-luebeck.de>
2021-06-09 20:20:06 +0200

    added BoardEvaluation-Test

--- src/test/java/chess/engine/EvaluatePiecesTest.java
+++ src/test/java/chess/engine/EvaluatePiecesTest.java
@@ -1,6 +1,9 @@
 package chess.engine;
 
+import chess.game.Colour;
 import chess.game.Game;
+import chess.pieces.King;
+import chess.pieces.Rook;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -23,5 +26,22 @@ public class EvaluatePiecesTest {
     public void evaluateBoard() {
         EvaluatePieces.evaluateBoard(game, game.currentPlayer.getColour());
         assertEquals(20, EvaluatePieces.evaluateBoard(game, game.currentPlayer.getColour()));
+
+    }
+
+    @Test
+    public void evaluateBoardWithOnlyKing(){
+        game.chessBoard.clearBoard();
+        game.chessBoard.clearBlackAlliance();
+        game.chessBoard.clearWhiteAlliance();
+        game.chessBoard.setPieceAt(0, 0, new King(game.chessBoard.getSquareAt(0, 0), Colour.WHITE));
+        game.chessBoard.setPieceAt(7, 0, new King(game.chessBoard.getSquareAt(7, 0), Colour.BLACK));
+        game.chessBoard.setPieceAt(1, 7, new Rook(game.chessBoard.getSquareAt(1, 7), Colour.BLACK));
+        game.chessBoard.setPieceAt(6, 1, new Rook(game.chessBoard.getSquareAt(6, 1), Colour.WHITE));
+        game.chessBoard.addWhiteAlliance(game.chessBoard.getPieceAt(0, 0));
+        game.chessBoard.addBlackAlliance(game.chessBoard.getPieceAt(7, 0));
+        game.chessBoard.addBlackAlliance(game.chessBoard.getPieceAt(1, 7));
+        game.chessBoard.addWhiteAlliance(game.chessBoard.getPieceAt(6, 1));
+        assertEquals(17, EvaluatePieces.evaluateBoard(game, game.currentPlayer.getColour()));
     }
 }
