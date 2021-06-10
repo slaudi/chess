297928d5eba148b3167daeac98c4d360455a9e0a
Christian Stelter <christian.stelter@student.uni-luebeck.de>
2021-06-10 13:35:18 +0200

    added evaluation- and engine- tests

--- src/test/java/chess/engine/EvaluatePiecesTest.java
+++ src/test/java/chess/engine/EvaluatePiecesTest.java
@@ -3,6 +3,8 @@ package chess.engine;
 import chess.game.Colour;
 import chess.game.Game;
 import chess.pieces.King;
+import chess.pieces.Piece;
+import chess.pieces.Queen;
 import chess.pieces.Rook;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -52,5 +54,40 @@ public class EvaluatePiecesTest {
         game.chessBoard.addBlackAlliance(game.chessBoard.getPieceAt(1, 7));
         game.chessBoard.addWhiteAlliance(game.chessBoard.getPieceAt(6, 1));
         assertEquals(17, EvaluatePieces.evaluateBoard(game, game.currentPlayer.getColour()));
+    }
+
+    /**
+     * an evaluation of a nearly empty board with only a King and a Rook for each Player. White King in Check.
+     */
+    @Test
+    public void evaluateBoardinCheck(){
+        game.chessBoard.clearBoard();
+        game.chessBoard.clearBlackAlliance();
+        game.chessBoard.clearWhiteAlliance();
+        game.chessBoard.setPieceAt(0, 0, new King(game.chessBoard.getSquareAt(0, 0), Colour.WHITE));
+        game.chessBoard.setPieceAt(7, 0, new King(game.chessBoard.getSquareAt(7, 0), Colour.BLACK));
+        game.chessBoard.setPieceAt(0, 7, new Rook(game.chessBoard.getSquareAt(0, 7), Colour.BLACK));
+        game.chessBoard.setPieceAt(6, 1, new Rook(game.chessBoard.getSquareAt(6, 1), Colour.WHITE));
+        game.chessBoard.addWhiteAlliance(game.chessBoard.getPieceAt(0, 0));
+        game.chessBoard.addBlackAlliance(game.chessBoard.getPieceAt(7, 0));
+        game.chessBoard.addBlackAlliance(game.chessBoard.getPieceAt(0, 7));
+        game.chessBoard.addWhiteAlliance(game.chessBoard.getPieceAt(6, 1));
+        assertEquals(117, EvaluatePieces.evaluateBoard(game, game.currentPlayer.getColour()));
+    }
+
+    /**
+     * an evaluation of a Queen.
+     */
+    @Test
+    public void piecesValueQueen(){
+        assertEquals(900, EvaluatePieces.piecesValue(new Queen(game.chessBoard.getSquareAt(5, 5), Colour.BLACK)));
+    }
+
+    /**
+     * an evaluation of an empty Square.
+     */
+    @Test
+    public void piecesValueNull(){
+        assertEquals(0, EvaluatePieces.piecesValue(null));
     }
 }
